41df12fc017117acbc5723b110a936c41086f3b3
Zixuan ZHU <208709518+zxzhuys@users.noreply.github.com>
2025-12-03 10:48:21 +0900

    Create result.py

---
+++ Week07/result.py
@@ -0,0 +1,21 @@
+- Start K-means Clustering -
+
+- Clustering Completed -
+3 Groups
+
+[Cluster 0] (Center: Station Distance - 0.9km, Passengers - 1.1million):
+  - Ginza Line
+  - Oedo Line
+  - Marunouchi Line
+
+[Cluster 1] (Center: Station Distance - 1.3km, Passengers - 3.9million):
+  - Yamanote Line
+  - Chuo Rapid Line
+
+[Cluster 2] (Center: Station Distance - 2.8km, Passengers - 0.4million):
+  - Keiyo Line
+  - Musashino Line
+  - Saikyo Line
+
+- Clustering Evaluation -
+Silhouette Score: 0.8320
