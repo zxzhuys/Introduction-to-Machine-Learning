de36284a3d3f189085999806fd8ae23f2d285bb5
Zixuan ZHU <208709518+zxzhuys@users.noreply.github.com>
2025-12-09 13:20:49 +0900

    Update result.py

--- Week08/result.py
+++ Week08/result.py
@@ -1,1 +1,159 @@
+# homework_week08_01.py
 
+Target Sentence: 'Hello, world!'
+- Start -
+Generation now 0: Best now: ' WKKGf jeSlZz' (Score: 2)
+Generation now 100: Best now: 'hKaQz, jeSlZh' (Score: 3)
+Generation now 200: Best now: 'hKaQz, jeNlZh' (Score: 3)
+Generation now 300: Best now: 'hKaQz, jeSlZh' (Score: 3)
+Generation now 400: Best now: 'hKaQz, YeSlrh' (Score: 3)
+Generation now 500: Best now: 'hKaQz, jerlZh' (Score: 4)
+Generation now 600: Best now: 'hKawz, jerlZh' (Score: 5)
+Generation now 700: Best now: 'hKaQz, jerlZ!' (Score: 5)
+Generation now 800: Best now: 'hKaQz, jerlZ!' (Score: 5)
+Generation now 900: Best now: 'hKaQz, jerlZ!' (Score: 5)
+Generation now 1000: Best now: 'hKaQz, jerlZ!' (Score: 5)
+Generation now 1100: Best now: 'hKaQz, jerlZ!' (Score: 5)
+Generation now 1200: Best now: 'hKaQz, jerlZ!' (Score: 5)
+Generation now 1300: Best now: 'hKaQz, jerlZ!' (Score: 5)
+Generation now 1400: Best now: 'hKaQz, jerlZ!' (Score: 5)
+Generation now 1500: Best now: 'hKaQz, jerlZ!' (Score: 5)
+Generation now 1600: Best now: 'hKaQz, jerlZ!' (Score: 5)
+Generation now 1700: Best now: 'hKaQz, jerlZ!' (Score: 5)
+Generation now 1800: Best now: 'hKaQz, jerlZ!' (Score: 5)
+Generation now 1900: Best now: 'hKaQz, jerlZ!' (Score: 5)
+Generation now 2000: Best now: 'hKaQz, jerlZ!' (Score: 5)
+Generation now 2100: Best now: 'hKaQz, jerlZ!' (Score: 5)
+Generation now 2200: Best now: 'hKaQz, jerlZ!' (Score: 5)
+Generation now 2300: Best now: 'HpaQz, werlZh' (Score: 6)
+Generation now 2400: Best now: 'HpaQz, werlZh' (Score: 6)
+Generation now 2500: Best now: 'HpaQz, werlZh' (Score: 6)
+Generation now 2600: Best now: 'HpkQI, werlZh' (Score: 6)
+Generation now 2700: Best now: 'HqlQI, werlZG' (Score: 8)
+Generation now 2800: Best now: 'HplQz, werldG' (Score: 8)
+Generation now 2900: Best now: 'HplQz, werldG' (Score: 8)
+Generation now 3000: Best now: 'HplQz, werldG' (Score: 8)
+Generation now 3100: Best now: 'HelQz, werldG' (Score: 9)
+Generation now 3200: Best now: 'HelQz, werldG' (Score: 9)
+Generation now 3300: Best now: 'HelQz, werldG' (Score: 9)
+Generation now 3400: Best now: 'HelQz, werldT' (Score: 10)
+Generation now 3500: Best now: 'HelQz, worldG' (Score: 10)
+Generation now 3600: Best now: 'HelQz, worldG' (Score: 11)
+Generation now 3700: Best now: 'HelQo, worldG' (Score: 11)
+Generation now 3800: Best now: 'HelQo, worldG' (Score: 11)
+Generation now 3900: Best now: 'Hello, worldG' (Score: 12)
+Generation now 4000: Best now: 'Hello, worldG' (Score: 12)
+Generation now 4100: Best now: 'Hello, worldl' (Score: 12)
+Generation now 4200: Best now: 'Hello, worldG' (Score: 12)
+Generation now 4300: Best now: 'Hello, worldG' (Score: 12)
+Generation now 4400: Best now: 'Hello, worldG' (Score: 12)
+Generation now 4500: Best now: 'Hello, worldB' (Score: 12)
+Generation now 4600: Best now: 'Hello, worldG' (Score: 12)
+Generation now 4700: Best now: 'Hello, worldG' (Score: 12)
+Generation now 4800: Best now: 'Hello, worldG' (Score: 12)
+Generation now 4900: Best now: 'Hello, worldG' (Score: 12)
+
+---
+Solution found at Generation 4918
+Final Solution: Hello, world!
+---
+
+
+# homework_week08_02.py
+
+- Start -
+Target Sentence: 'Jedi'
+Population Size: 20
+Mutation Rate: 0.5
+Generation now 0: Best now='heKJ' (Score: 1/4)
+Generation now 500: Best now='Jedz' (Score: 3/4)
+Generation now 1000: Best now='Jedw' (Score: 3/4)
+Solution found at Generation 1260
+time: 0.0216s
+Final Solution: 'Jedi'
+Test 1 Finish
+
+
+- Start -
+Target Sentence: 'Star Wars'
+Population Size: 100
+Mutation Rate: 0.4
+Generation now 0: Best now='LKArYuaEf' (Score: 2/9)
+Generation now 500: Best now='SIGrnWaOH' (Score: 4/9)
+Generation now 1000: Best now='SIFrYWaBH' (Score: 4/9)
+Generation now 1500: Best now='SEarYWaBs' (Score: 6/9)
+Generation now 2000: Best now='SEarYWaBs' (Score: 6/9)
+Generation now 2500: Best now='Star WaBV' (Score: 7/9)
+Generation now 3000: Best now='Star WaBw' (Score: 7/9)
+Generation now 3500: Best now='Star WaBV' (Score: 7/9)
+Solution found at Generation 3713
+time: 0.3022s
+Final Solution: 'Star Wars'
+Test 2 Finish
+
+
+- Start -
+Target Sentence: 'May the force be with you, young Skywalker.'
+Population Size: 200
+Mutation Rate: 0.3
+Generation now 0: Best now='swXpCneDloOlCFceAnsIuMhignEOQjDF!S?OCp!FhWn' (Score: 4/43)
+Generation now 500: Best now='slX CneDloEgpFGeuFBvfAGBJg MyeFXcSLrhP.OFrn' (Score: 7/43)
+Generation now 1000: Best now='swX CneDloIgpFGeuFBvfAGBJi MyenXcSLyhP.OFrn' (Score: 9/43)
+Generation now 1500: Best now='swj CneDloEgpFXeuFBvfAGBJi MyenXcSLyhP.OFr.' (Score: 10/43)
+Generation now 2000: Best now='swj CneDloEgpFXeuFBvfAGBJi MyenXcSLyhP.OFr.' (Score: 10/43)
+Generation now 2500: Best now='swj CneDloEgpFXeuFBvfAGBJi MyenXcSLyhP.OFr.' (Score: 10/43)
+Generation now 3000: Best now='swj tneDloEgpFXeuFBvfAGYJi MyenXcSkyhP.OFr.' (Score: 12/43)
+Generation now 3500: Best now='swj tneDloEgpFXeuFBvfAGBJi MoenXcSkyhP.OFr.' (Score: 13/43)
+Generation now 4000: Best now='swj tneDloEgpFXeuFivf GBJi MoenXcSkyhP.OFr.' (Score: 15/43)
+Generation now 4500: Best now='swj tneDloEgpFXeuFivf GBJi MoenXcSkyhP.OFr.' (Score: 15/43)
+Generation now 5000: Best now='saj tneDloEgpFXeuwivf GBJi MoenXcSkyhP.OFr.' (Score: 17/43)
+Generation now 5500: Best now='saj tneDloEgpFXeuwivf GBJi MounXcSkyhPlOFr.' (Score: 19/43)
+Generation now 6000: Best now='saj tneDloEgpFXeuwivf yBJi MounXcSkyhPlOFrn' (Score: 19/43)
+Generation now 6500: Best now='saj tneDloEgpFXeuwivf yBJi younXcSkyhPlOFrn' (Score: 20/43)
+Generation now 7000: Best now='saj tneDloEgpFXeuwitf yBJ, younXcSkywPlOFrn' (Score: 23/43)
+Generation now 7500: Best now='saj tne loEgpFXe witf yBJ, younXcSkywPlOFr?' (Score: 25/43)
+Generation now 8000: Best now='saj tne loEgpFXe witf yBJ, younXcSkywHlOer?' (Score: 26/43)
+Generation now 8500: Best now='Maj tne loEgpFXe witf yBJ, younXcSkywHlOer?' (Score: 27/43)
+Generation now 9000: Best now='Maj tne loEgeFXe witf yBJ, younXcSkywHlker?' (Score: 29/43)
+Generation now 9500: Best now='Maj tne loEge Xe witb yBJ, younX SkywHlker?' (Score: 31/43)
+Generation now 10000: Best now='Maj tne loEge Xe witb yBJ, younX SkywHlker.' (Score: 32/43)
+Generation now 10500: Best now='Maj tne loEge Xe witb yBJ, younX SkywHlker.' (Score: 32/43)
+Generation now 11000: Best now='Maj tne OoEce Xe witb yBJ, younX SkywHlker.' (Score: 33/43)
+Generation now 11500: Best now='Maj tne OoEce Xe witb yoJ, young SkywHlker.' (Score: 35/43)
+Generation now 12000: Best now='Maj tne OoEce Xe witb yoJ, young SkywHlker.' (Score: 35/43)
+Generation now 12500: Best now='Maj tne zoEce Xe witb yoJ, young SkywHlker.' (Score: 35/43)
+Generation now 13000: Best now='Maj tne OoEce  e witb yoJ, young SkywHlker.' (Score: 35/43)
+Generation now 13500: Best now='Maj tne OoEce Xe witb yoJ, young SkywHlker.' (Score: 35/43)
+Generation now 14000: Best now='May tne foEce  e witb yoJ, young SkywHlker.' (Score: 37/43)
+Generation now 14500: Best now='May tne foEce  e witb yoJ, young SkywHlker.' (Score: 37/43)
+Generation now 15000: Best now='May tne foEce  e with yoJ, young SkywHlker.' (Score: 38/43)
+Generation now 15500: Best now='May tne foEce be with yoJ, young SkywHlker.' (Score: 39/43)
+Generation now 16000: Best now='May tne foEce be with yoJ, young SkywHlker.' (Score: 39/43)
+Generation now 16500: Best now='May tne force be with yoJ, young SkywHlker.' (Score: 40/43)
+Generation now 17000: Best now='May tne force be with yoJ, young SkywHlker.' (Score: 40/43)
+Generation now 17500: Best now='May tne force be with yoJ, young SkywHlker.' (Score: 40/43)
+Generation now 18000: Best now='May tne force be with yoJ, young SkywHlker.' (Score: 40/43)
+Generation now 18500: Best now='May tne force be with yoJ, young SkywHlker.' (Score: 40/43)
+Generation now 19000: Best now='May tne force be with yoJ, young SkywHlker.' (Score: 40/43)
+Generation now 19500: Best now='May the force be with yoJ, young SkywTlker.' (Score: 41/43)
+Generation now 20000: Best now='May the force be with yoJ, young SkywTlker.' (Score: 41/43)
+Generation now 20500: Best now='May the force be with yoJ, young SkywTlker.' (Score: 41/43)
+Generation now 21000: Best now='May the force be with yoJ, young Skywalker.' (Score: 42/43)
+Generation now 21500: Best now='May the force be with yoJ, young Skywalker.' (Score: 42/43)
+Generation now 22000: Best now='May the force be with yoJ, young Skywalker.' (Score: 42/43)
+Generation now 22500: Best now='May the force be with yoJ, young Skywalker.' (Score: 42/43)
+Generation now 23000: Best now='May the force be with yoJ, young Skywalker.' (Score: 42/43)
+Generation now 23500: Best now='May the force be with yoJ, young Skywalker.' (Score: 42/43)
+Generation now 24000: Best now='May the force be with yoJ, young Skywalker.' (Score: 42/43)
+Generation now 24500: Best now='May the force be with yoJ, young Skywalker.' (Score: 42/43)
+Generation now 25000: Best now='May the force be with yoJ, young Skywalker.' (Score: 42/43)
+Generation now 25500: Best now='May the force be with yoJ, young Skywalker.' (Score: 42/43)
+Generation now 26000: Best now='May the force be with yoJ, young Skywalker.' (Score: 42/43)
+Generation now 26500: Best now='May the force be with yoJ, young Skywalker.' (Score: 42/43)
+Generation now 27000: Best now='May the force be with yoJ, young Skywalker.' (Score: 42/43)
+Generation now 27500: Best now='May the force be with yoJ, young Skywalker.' (Score: 42/43)
+Generation now 28000: Best now='May the force be with yoJ, young Skywalker.' (Score: 42/43)
+Solution found at Generation 28240
+time: 10.9828s
+Final Solution: 'May the force be with you, young Skywalker.'
+Test 3 Finish
