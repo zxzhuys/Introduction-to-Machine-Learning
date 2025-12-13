f292f2b7c6edad44d4bf58f3a4ad39bba620471c
Zixuan ZHU <208709518+zxzhuys@users.noreply.github.com>
2025-12-13 15:19:53 +0900

    Update result.py

--- Week09/result.py
+++ Week09/result.py
@@ -1,1 +1,11 @@
-
+                                               title  Correlation  ratings_count  movieId                       genres
+0                        Sleepless in Seattle (1993)     1.000000            106      539         Comedy|Drama|Romance
+1                                      Clerks (1994)     1.000000            104      223                       Comedy
+2                                  Braveheart (1995)     0.969345            237      110             Action|Drama|War
+3  Crouching Tiger, Hidden Dragon (Wo hu cang lon...     0.960769            110     3996         Action|Drama|Romance
+4                             American Beauty (1999)     0.951779            204     2858                Drama|Romance
+5                   Shawshank Redemption, The (1994)     0.930106            317      318                  Crime|Drama
+6                         Clockwork Orange, A (1971)     0.917663            120     1206  Crime|Drama|Sci-Fi|Thriller
+7                                Pretty Woman (1990)     0.888459            135      597               Comedy|Romance
+8                                       Fargo (1996)     0.784465            181      608  Comedy|Crime|Drama|Thriller
+9                                  Fight Club (1999)     0.775106            218     2959  Action|Crime|Drama|Thriller
