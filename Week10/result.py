c46cee1aba32847bc3784a022ed025d2a6ad5bad
Zixuan ZHU <208709518+zxzhuys@users.noreply.github.com>
2025-12-18 15:41:06 +0900

    Update result.py

--- Week10/result.py
+++ Week10/result.py
@@ -1,1 +1,32 @@
-
+Epoch 1/10
+1688/1688 [==============================] - 3s 2ms/step - loss: 0.2945 -
+accuracy: 0.9131 - val_loss: 0.1515 - val_accuracy: 0.9568
+Epoch 2/10
+1688/1688 [==============================] - 3s 2ms/step - loss: 0.1654 -
+accuracy: 0.9521 - val_loss: 0.1479 - val_accuracy: 0.9620
+Epoch 3/10
+1688/1688 [==============================] - 3s 2ms/step - loss: 0.1221 -
+accuracy: 0.9642 - val_loss: 0.1065 - val_accuracy: 0.9707
+Epoch 4/10
+1688/1688 [==============================] - 3s 2ms/step - loss: 0.1013 -
+accuracy: 0.9699 - val_loss: 0.1104 - val_accuracy: 0.9725
+Epoch 5/10
+1688/1688 [==============================] - 3s 2ms/step - loss: 0.0875 -
+accuracy: 0.9737 - val_loss: 0.1246 - val_accuracy: 0.9690
+Epoch 6/10
+1688/1688 [==============================] - 3s 2ms/step - loss: 0.0792 -
+accuracy: 0.9759 - val_loss: 0.1071 - val_accuracy: 0.9737
+Epoch 7/10
+1688/1688 [==============================] - 3s 2ms/step - loss: 0.0691 -
+accuracy: 0.9790 - val_loss: 0.1055 - val_accuracy: 0.9737
+Epoch 8/10
+1688/1688 [==============================] - 3s 2ms/step - loss: 0.0576 -
+accuracy: 0.9829 - val_loss: 0.0994 - val_accuracy: 0.9765
+Epoch 9/10
+1688/1688 [==============================] - 4s 2ms/step - loss: 0.0516 -
+accuracy: 0.9839 - val_loss: 0.1042 - val_accuracy: 0.9733
+Epoch 10/10
+1688/1688 [==============================] - 4s 3ms/step - loss: 0.0525 -
+accuracy: 0.9832 - val_loss: 0.1066 - val_accuracy: 0.9763
+313/313 - 0s - loss: 0.1009 - accuracy: 0.9730 - 445ms/epoch - 1ms/step
+Test accuracy: 0.9729999899864197
