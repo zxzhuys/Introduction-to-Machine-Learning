0642b5b5de9bf8bdfdd374b9fab2b5c3cd34cfc7
Zixuan ZHU <208709518+zxzhuys@users.noreply.github.com>
2025-12-25 14:02:41 +0900

    Update result.py

--- Week11/result.py
+++ Week11/result.py
@@ -1,1 +1,30 @@
-
+Epoch 1/15
+782/782 [==============================] - 6s 7ms/step - loss: 1.6737 - accuracy: 0.3748 - val_loss: 1.3578 - val_accuracy: 0.5052
+Epoch 2/15
+782/782 [==============================] - 5s 7ms/step - loss: 1.2702 - accuracy: 0.5399 - val_loss: 1.2017 - val_accuracy: 0.5735
+Epoch 3/15
+782/782 [==============================] - 5s 7ms/step - loss: 1.1163 - accuracy: 0.6016 - val_loss: 1.0930 - val_accuracy: 0.6098
+Epoch 4/15
+782/782 [==============================] - 5s 7ms/step - loss: 1.0085 - accuracy: 0.6449 - val_loss: 1.1094 - val_accuracy: 0.6121
+Epoch 5/15
+782/782 [==============================] - 5s 7ms/step - loss: 0.9390 - accuracy: 0.6674 - val_loss: 1.0408 - val_accuracy: 0.6318
+Epoch 6/15
+782/782 [==============================] - 5s 7ms/step - loss: 0.8790 - accuracy: 0.6900 - val_loss: 0.9732 - val_accuracy: 0.6575
+Epoch 7/15
+782/782 [==============================] - 5s 7ms/step - loss: 0.8299 - accuracy: 0.7101 - val_loss: 0.9923 - val_accuracy: 0.6627
+Epoch 8/15
+782/782 [==============================] - 5s 7ms/step - loss: 0.7889 - accuracy: 0.7229 - val_loss: 0.9708 - val_accuracy: 0.6688
+Epoch 9/15
+782/782 [==============================] - 5s 7ms/step - loss: 0.7570 - accuracy: 0.7338 - val_loss: 0.9363 - val_accuracy: 0.6774
+Epoch 10/15
+782/782 [==============================] - 5s 7ms/step - loss: 0.7184 - accuracy: 0.7503 - val_loss: 0.9625 - val_accuracy: 0.6806
+Epoch 11/15
+782/782 [==============================] - 5s 7ms/step - loss: 0.6968 - accuracy: 0.7561 - val_loss: 0.9140 - val_accuracy: 0.6891
+Epoch 12/15
+782/782 [==============================] - 5s 7ms/step - loss: 0.6702 - accuracy: 0.7648 - val_loss: 0.9982 - val_accuracy: 0.6778
+Epoch 13/15
+782/782 [==============================] - 5s 7ms/step - loss: 0.6435 - accuracy: 0.7766 - val_loss: 0.9234 - val_accuracy: 0.6926
+Epoch 14/15
+782/782 [==============================] - 5s 7ms/step - loss: 0.6115 - accuracy: 0.7864 - val_loss: 0.9518 - val_accuracy: 0.6941
+Epoch 15/15
+782/782 [==============================] - 5s 7ms/step - loss: 0.5941 - accuracy: 0.7922 - val_loss: 0.9574 - val_accuracy: 0.6901
