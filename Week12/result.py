ea16466d7e6bcda34b2c968f9e70ee248a1c76f4
Zixuan ZHU <208709518+zxzhuys@users.noreply.github.com>
2026-01-08 19:17:25 +0900

    Update result.py

--- Week12/result.py
+++ Week12/result.py
@@ -1,1 +1,18 @@
-
+Epoch 1/5
+469/469 [==============================] - 15s 28ms/step - loss: 0.6572 -
+accuracy: 0.7800 - val_loss: 0.2626 - val_accuracy: 0.9172
+Epoch 2/5
+469/469 [==============================] - 13s 28ms/step - loss: 0.1730 -
+accuracy: 0.9475 - val_loss: 0.1442 - val_accuracy: 0.9553
+Epoch 3/5
+469/469 [==============================] - 13s 29ms/step - loss: 0.1169 -
+accuracy: 0.9649 - val_loss: 0.0910 - val_accuracy: 0.9700
+Epoch 4/5
+469/469 [==============================] - 13s 29ms/step - loss: 0.0908 -
+accuracy: 0.9724 - val_loss: 0.0850 - val_accuracy: 0.9718
+Epoch 5/5
+469/469 [==============================] - 13s 29ms/step - loss: 0.0742 -
+accuracy: 0.9770 - val_loss: 0.0640 - val_accuracy: 0.9799
+469/469 [==============================] - 4s 9ms/step - loss: 0.0568 -
+accuracy: 0.9829
+Test accuracy: 0.9828500151634216
